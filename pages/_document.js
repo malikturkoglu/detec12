27dc057f7a20a58c2abaf94726546e08aa8a5cf2
malik türkoğlu <52261664+malikturkoglu@users.noreply.github.com>
2023-04-15 23:34:10 +0300

    script changed

--- pages/_document.js
+++ pages/_document.js
@@ -8,16 +8,18 @@ export default function Document() {
     <Html lang="en">
       <Head/>
       <body>
-      <Script src="/ym.js"></Script>  
+     
         <Main />
         <NextScript />
-        <footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
-    <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a class="hover:underline">ZEMHEREX</a>. All Rights Reserved. 
+        <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+    <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+      <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a className="hover:underline">ZEMHEREX</a>. All Rights Reserved. 
       This webpage is only accessible to project workers. Please exit the site if you are not a project employee. 
       The site s new features may be troublesome for you. M
     </span>
     </div>
+    <script src="/ym.js"/>
+    <div><img src="https://mc.yandex.ru/watch/93234692" style={{ position:"absolute" , left:"-9999px"}} alt="" /></div>
 </footer>
       </body>
     </Html>
