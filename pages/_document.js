23a5218b90a4bd4458e30fea71c5a47f20d33e87
malik türkoğlu <52261664+malikturkoglu@users.noreply.github.com>
2023-04-15 20:20:22 +0300

    Script Changed

--- pages/_document.js
+++ pages/_document.js
@@ -2,12 +2,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 import Script from 'next/script'
 
 
+
 export default function Document() {
   return (
     <Html lang="en">
       <Head/>
       <body>
-      <script src="/ym.js"></script>  
+      <Script src="/ym.js"></Script>  
         <Main />
         <NextScript />
         <footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
