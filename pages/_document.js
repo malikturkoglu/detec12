dc2b6c984701758b6a7f7d89f41e9f8af7f36342
malik türkoğlu <52261664+malikturkoglu@users.noreply.github.com>
2023-04-15 19:40:01 +0300

    metrica

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,13 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
+
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head/>
       <body>
+      <script src="/ym.js"></script>  
         <Main />
         <NextScript />
         <footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
